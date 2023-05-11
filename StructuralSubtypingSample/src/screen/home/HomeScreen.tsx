165eb2ceef0b0a58dfc1626679a5d1e6bfde6f52
Kaito-Dogi <kendobu0405@gmail.com>
2023-05-12 00:11:58 +0900

    💄 HomeScreen

--- StructuralSubtypingSample/src/screen/home/HomeScreen.tsx
+++ StructuralSubtypingSample/src/screen/home/HomeScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {LargeListItem} from 'src/component/LargeListItem';
+import {ListItem} from 'src/component/ListItem';
+import {MultiColumnListItem} from 'src/component/MultiColumnListItem';
+
+import {styles} from './styles';
 
 /**
  * @package
  */
 export const HomeScreen: React.FC = () => (
-  <View>
-    <Text>HomeScreen</Text>
+  <View style={styles.container}>
+    <LargeListItem text="L" />
+    <ListItem text="N" />
+    <MultiColumnListItem texts={['M1', 'M2']} />
   </View>
 );
