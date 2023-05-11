6ef0c3534ba02a1e8fec237283d8ee5a62b0326d
Kaito-Dogi <kendobu0405@gmail.com>
2023-05-11 20:18:19 +0900

    ➕ ESLint 関連

--- StructuralSubtypingSample/.eslintrc.js
+++ StructuralSubtypingSample/.eslintrc.js
@@ -2,7 +2,22 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+    'simple-import-sort',
+    'sort-destructure-keys',
+    'import-access',
+    'unused-imports',
+  ],
+  rules: {
+    // sort
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'sort-destructure-keys/sort-destructure-keys': 2,
+    // import/export
+    'unused-imports/no-unused-imports': 'error',
+    'import-access/jsdoc': 'error',
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
