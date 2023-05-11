94290432e40b89280f2cc9fd6effcd176e6a598c
Kaito-Dogi <kendobu0405@gmail.com>
2023-05-11 19:26:14 +0900

    first commit

---
+++ nominal-typing-sample/app/src/main/java/app/doggy/nominaltypingsample/MainActivity.kt
@@ -0,0 +1,16 @@
+package app.doggy.nominaltypingsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MaterialTheme {
+      }
+    }
+  }
+}
