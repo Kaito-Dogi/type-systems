dd53c4c767d00f92551f7eab7939d7dae9a8ee8d
Kaito-Dogi <kendobu0405@gmail.com>
2023-05-12 01:04:07 +0900

    :lipstick: ListItem

---
+++ NominalTypingSample/app/src/main/java/app/doggy/nominaltypingsample/ui/component/ListItem.kt
@@ -0,0 +1,39 @@
+package app.doggy.nominaltypingsample.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ListItem(
+  text: String,
+  modifier: Modifier = Modifier,
+) {
+  Box(
+    modifier = modifier
+      .fillMaxWidth()
+      .height(64.dp)
+      .background(Color.Green),
+    contentAlignment = Alignment.Center,
+  ) {
+    Text(text = text)
+  }
+}
+
+@Preview
+@Composable
+private fun ListItemPreview() {
+  MaterialTheme {
+    ListItem(text = "ListItem")
+  }
+}
